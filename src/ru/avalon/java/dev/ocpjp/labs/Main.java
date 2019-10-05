bb7dba6fc908e394d80f155d9eddd774833fd04a
moskovanton <48486058+moskovanton@users.noreply.github.com>
2019-10-05 12:32:43 +0300

    Update Main.java

--- src/ru/avalon/java/dev/ocpjp/labs/Main.java
+++ src/ru/avalon/java/dev/ocpjp/labs/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         final Collection<Commodity> commodities = Commodity.random(100);
-
+      
         /*
          * TODO(Студент): С использованием Java Stream API выполнить задачи из списка:
          * 1. Выполнить поиск самого дорого товара
@@ -21,5 +21,21 @@ public class Main {
          * 7. Найти среднюю стоимость товаров
          * 8. Найти моду (медиану) стоимости товаров
          */
+        
+        System.out.println("Cамый дорогой товар: " + commodities.stream().map((s)-> s.getPrice()).max(Double::compare).toString());
+        
+        System.out.println("Товары с минимальным остатком: " + commodities.stream().map((s)-> s.getResidue()).min(Integer::compare).toString());
+        
+        System.out.print("Товары с остатком меньшим 5: ");
+        commodities.stream().filter((s) -> (s.getResidue()) < 5).forEach((s) -> System.out.println(s.getName()));
+                                
+        System.out.println("Общая стоимость товаров с учётом их остатка: " + commodities.stream().mapToDouble((s)-> (s.getResidue() * s.getPrice())).sum());
+                
+        System.out.println("Товар с самым длинным названием: " + commodities.stream().mapToInt((s)-> s.getName().length()).max());
+                 
+        System.out.println("Сортировка коллекции commodities: " + commodities.stream().sorted());
+    
+        System.out.println("Средняя стоимость товаров: " + commodities.stream().mapToDouble((s)-> s.getPrice()).average());
+ 
     }
 }
